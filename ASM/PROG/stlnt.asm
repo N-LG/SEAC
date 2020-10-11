7b3579870feebb0a497bf8356feb48fe0d741d54
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2020-10-11 18:38:55 +0200

    2 ajout + suppression conversion page de code 850
    ajout message envoyé aux nouveaux connecté
    ajout possibilité choisir port utilisé
    abandon support client telnet windows parce que c'est de la merde

--- ASM/PROG/stlnt.asm
+++ ASM/PROG/stlnt.asm
@@ -6,8 +6,7 @@ scode:
 org 0
 
 ;choses encore a faire:
-;gestion des codes d'échapement  dans les inputs
-;ouverture d'un autre port que le standard
+;demande mot de passe a la connexion
 
 
 taille_journal equ 10000h
@@ -19,12 +18,36 @@ mov ds,ax
 mov es,ax
 
 mov ecx,finzt
-mov dx,sel_dat2
+mov dx,sel_dat1
 mov al,8
 int 61h
 
 
+;**************************************************************
+;selectionne port a utiliser
+mov byte[zt_transfert],0
 
+mov al,4   
+mov ah,1   ;numéros de l'option de commande a lire
+mov cl,0 ;0=256 octet max
+mov edx,zt_transfert
+int 61h
+
+cmp byte[zt_transfert],0
+je ignore_choix_port
+
+mov al,100  
+mov edx,zt_transfert
+int 61h
+
+cmp ecx,0
+je erreur_param
+test ecx,0FFFF0000h
+jnz erreur_param
+
+mov [port],cx
+
+ignore_choix_port:
 
 
 ;**************************************************************
@@ -66,11 +89,12 @@ mov edi,0
 int 65h
 mov [adresse_canal],ebx
 
-;configure en écoute pour le port TCP23
+;configure en écoute pour le port TCP23 (ou celuis choisi dans la commande)
+mov ax,[port]
 mov word[zt_transfert],8      ;ouverture port tcp
-mov word[zt_transfert+2],23
+mov word[zt_transfert+2],ax
 mov word[zt_transfert+4],12 ;connexion max
-mov dword[zt_transfert+6],4096 ;taille de la zt de communication a reserver a chaque canal de communication
+mov dword[zt_transfert+6],taille_journal ;taille de la zt de communication a reserver a chaque canal de communication
 
 mov al,5
 mov ebx,[adresse_canal]
@@ -180,6 +204,12 @@ call formatage
 mov al,7
 int 65h
 
+;..un petit message de bienvenu
+mov al,7
+mov ecx,msgbasefin-msgbase
+mov esi,msgbase
+int 65h
+
 ;..l'invite de commande
 mov al,7
 mov ecx,3
@@ -202,45 +232,29 @@ mov edx,zt_journal1
 mov ecx,taille_journal
 int 61h
 mov byte[ecx+zt_journal1],0
+inc ecx
 mov [taille_journal1],ecx
-
+cmp ecx,1
+je sauvgarde_journal
 
 ;compare avec le dernier journal pour voir si quelquechose as été modifié
 mov esi,zt_journal1
 mov edi,zt_journal2
-mov ecx,taille_journal
+mov ecx,[taille_journal1]
 cld
 repe cmpsb
-
+cmp ecx,0
+je partie3
 dec edi
 dec esi
-cmp byte[esi],0
-je partie3
 
 ;efface la ligne de commande
 call efface
 
 ;envoie la dernières parties modifé aux correspondants
-mov ecx,esi
-boucle3:
-inc ecx
-cmp byte[ecx],0
-jne boucle3
-sub ecx,esi
 call formatage
 call envoie_massif
   
-
-;sauvegarde le dernier journal modifié
-mov esi,zt_journal1
-mov edi,zt_journal2
-mov ecx,taille_journal
-cld
-rep movsb
-mov ecx,[taille_journal1]
-mov [taille_journal2],ecx
-
-
 ;reécrit la ligne de commande
 mov ecx,3
 mov esi,debut_cmd
@@ -248,6 +262,17 @@ call envoie_massif
 mov esi,zt_commande
 mov ecx,[offset]
 call envoie_massif
+
+
+;sauvegarde le dernier journal modifié
+sauvgarde_journal:
+mov esi,zt_journal1
+mov edi,zt_journal2
+mov ecx,taille_journal
+cld
+rep movsb
+mov eax,[taille_journal1]
+mov [taille_journal2],eax
 
 
 ;***************************************************************************************************************************************************
@@ -277,6 +302,8 @@ cmp al,13
 je commandetrouve
 cmp al,1Bh      ;escape code
 je ignore_carac
+cmp al,7Fh
+je annulcarac
 cmp al,0FFh     ;interprete as command
 je interpretascommand
 cmp al,20
@@ -417,16 +444,21 @@ cmp al,13
 je crlf_formatage
 cmp al,20h
 jb suite_formatage
-test al,080h
-jz lutf1ch
-test al,040h
-jz suite_formatage
-test al,020h
-jz lutf2ch
-test al,010h
-jz lutf3ch
-test al,08h
-jz lutf4ch
+cmp al,0FFh
+je suite_formatage 
+;test al,080h
+;jz lutf1ch
+;test al,040h
+;jz suite_formatage
+;test al,020h
+;jz lutf2ch
+;test al,010h
+;jz lutf3ch
+;test al,08h
+;jz lutf4ch
+
+mov [edi],al
+inc edi
 
 suite_formatage:
 inc esi
@@ -444,93 +476,79 @@ add edi,2
 jmp suite_formatage
 
 
-lutf1ch:
-mov [edi],al
-inc edi
-jmp suite_formatage
+;lutf1ch:
+;mov [edi],al
+;inc edi
+;jmp suite_formatage
 
 
-lutf2ch:
-xor eax,eax
-mov al,[esi]
-and al,1Fh
-shl eax,6
-mov dl,[esi+1]
-and dl,3Fh
-or al,dl
-jmp correspondance
+;lutf2ch:
+;xor eax,eax
+;mov al,[esi]
+;and al,1Fh
+;shl eax,6
+;mov dl,[esi+1]
+;and dl,3Fh
+;or al,dl
+;jmp correspondance
 
-lutf3ch:
-xor eax,eax
-mov al,[esi]
-and al,0Fh
-shl eax,6
-mov dl,[esi+1]
-and dl,3Fh
-or al,cl
-shl eax,6
-mov dl,[esi+2]
-and dl,3Fh
-or al,dl
-jmp correspondance
+;lutf3ch:
+;xor eax,eax
+;mov al,[esi]
+;and al,0Fh
+;shl eax,6
+;mov dl,[esi+1]
+;and dl,3Fh
+;or al,cl
+;shl eax,6
+;mov dl,[esi+2]
+;and dl,3Fh
+;or al,dl
+;jmp correspondance
 
-lutf4ch:
-xor eax,eax
-mov al,[esi]
-and al,07h
-shl eax,6
-mov dl,[esi+1]
-and dl,3Fh
-or al,dl
-shl eax,6
-mov dl,[esi+2]
-and dl,3Fh
-or al,dl
-shl eax,6
-mov dl,[esi+3]
-and dl,3Fh
-or al,dl
-correspondance:
+;lutf4ch:
+;xor eax,eax
+;mov al,[esi]
+;and al,07h
+;shl eax,6
+;mov dl,[esi+1]
+;and dl,3Fh
+;or al,dl
+;shl eax,6
+;mov dl,[esi+2]
+;and dl,3Fh
+;or al,dl
+;shl eax,6
+;mov dl,[esi+3]
+;and dl,3Fh
+;or al,dl
+;correspondance:
 
-test eax,0FFFF0000h
-jnz remplacement
+;test eax,0FFFF0000h
+;jnz remplacement
 
-mov edx,code850
-boucleap:
-cmp [edx],ax
-je trouve
-add edx,2
-cmp edx,code850+256
-jne boucleap
+;mov edx,code850
+;boucleap:
+;cmp [edx],ax
+;je trouve
+;add edx,2
+;cmp edx,code850+256
+;jne boucleap
 
-remplacement:
-mov byte[edi],0B0h ;carré a la place des caractères non affichable
-inc edi
-jmp suite_formatage
+;remplacement:
+;mov byte[edi],0B0h ;carré a la place des caractères non affichable
+;inc edi
+;jmp suite_formatage
 
-trouve:
-sub edx,code850
-shr edx,1
-add dl,80h
-cmp dl,0FFh
-je remplacement
-mov [edi],dl
-inc edi
-jmp suite_formatage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+;trouve:
+;sub edx,code850
+;shr edx,1
+;add dl,80h
+;cmp dl,0FFh
+;je remplacement
+;mov [edi],dl
+;inc edi
+;jmp suite_formatage
 
 
 ;***********************************
@@ -553,17 +571,22 @@ org 0
 
 
 
-code850:
-dw 00C7h,00FCh,00E9h,00E2h,00E4h,00E0h,00E5h,00E7h,00EAh,00EBh,00E8h,00EFh,00EEh,00ECh,00C4h,00C5h
-dw 00C9h,00E6h,00C6h,00F4h,00F6h,00F2h,00FBh,00F9h,00FFh,00D6h,00DCh,00F8h,00A3h,00D8h,00D7h,0192h
-dw 00E1h,00EDh,00F3h,00FAh,00F1h,00D1h,00AAh,00BAh,00BFh,00AEh,00ACh,00BDh,00BCh,00A1h,00ABh,00BBh
-dw 2591h,2592h,2593h,2502h,2524h,00C1h,00C2h,00C0h,00A9h,2563h,2551h,2557h,255Dh,00A2h,00A5h,2510h
-dw 2514h,2534h,252Ch,251Ch,2500h,253Ch,00E3h,00C3h,255Ah,2554h,2569h,2566h,2560h,2550h,256Ch,00A4h
-dw 00F0h,00D0h,00CAh,00CBh,00C8h,0131h,00CDh,00CEh,00CFh,2518h,250Ch,2588h,2584h,00A6h,00CCh,2580h
-dw 00D3h,00DFh,00D4h,00D2h,00F5h,00D5h,00B5h,00FEh,00DEh,00DAh,00DBh,00D9h,00FDh,00DDh,00AFh,00B4h
-dw 00ADh,00B1h,2017h,00BEh,00B6h,00A7h,00F7h,00B8h,00B0h,00A8h,00B7h,00B9h,00B3h,00B2h,25A0h,00A0h
+;code850:
+;dw 00C7h,00FCh,00E9h,00E2h,00E4h,00E0h,00E5h,00E7h,00EAh,00EBh,00E8h,00EFh,00EEh,00ECh,00C4h,00C5h
+;dw 00C9h,00E6h,00C6h,00F4h,00F6h,00F2h,00FBh,00F9h,00FFh,00D6h,00DCh,00F8h,00A3h,00D8h,00D7h,0192h
+;dw 00E1h,00EDh,00F3h,00FAh,00F1h,00D1h,00AAh,00BAh,00BFh,00AEh,00ACh,00BDh,00BCh,00A1h,00ABh,00BBh
+;dw 2591h,2592h,2593h,2502h,2524h,00C1h,00C2h,00C0h,00A9h,2563h,2551h,2557h,255Dh,00A2h,00A5h,2510h
+;dw 2514h,2534h,252Ch,251Ch,2500h,253Ch,00E3h,00C3h,255Ah,2554h,2569h,2566h,2560h,2550h,256Ch,00A4h
+;dw 00F0h,00D0h,00CAh,00CBh,00C8h,0131h,00CDh,00CEh,00CFh,2518h,250Ch,2588h,2584h,00A6h,00CCh,2580h
+;dw 00D3h,00DFh,00D4h,00D2h,00F5h,00D5h,00B5h,00FEh,00DEh,00DAh,00DBh,00D9h,00FDh,00DDh,00AFh,00B4h
+;dw 00ADh,00B1h,2017h,00BEh,00B6h,00A7h,00F7h,00B8h,00B0h,00A8h,00B7h,00B9h,00B3h,00B2h,25A0h,00A0h
 
-
+msgbase:
+db "********************************************",10,13
+db "*  Bienvenue sur ce serveur de commande a  *",10,13
+db "* distance d'un syteme d'exploitation SEAC *",10,13
+db "********************************************",10,13
+msgbasefin:
 
 
 msgok:
@@ -576,16 +599,20 @@ msgnok1:
 db "STLNT: erreur lors de l'ouverture du port",13,0
 msgnok2:
 db "STLNT: erreur dans les parametre de la ligne de commande, format correcte:",13
-db "STFTP [X]",13
-db "[X] numéros de l'interface sur laquelle brancher le serveur TFTP",13,0
+db "STFTP [X] [Y]",13
+db "[X] numéros de l'interface sur laquelle brancher le serveur TFTP",13
+db "[Y] numéros du port a ouvrir (optionnel, si différent du port standard 23)",13,0 
 
 debut_cmd:
-db 0C0h,0C4h,">"
+db "-->"
+;db 0C0h,0C4h,">"
 
 nonecho:
 db 0ffh,0fbh,01h
 
 
+port:
+dw 23     ;port par défaut
 adresse_canal:
 dd 0
 offset:
