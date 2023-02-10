068d3510f873aae6589ea5bcbed0a533e309ad5e
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2023-02-10 02:10:25 +0100

    prise en compte des descripteur d'interface et du protocole

--- ASM/PROG/lsusb.asm
+++ ASM/PROG/lsusb.asm
@@ -154,113 +154,145 @@ and ecx,0FFFFh
 mov edx,id
 mov al,104
 int 61h
-mov byte[edx+4]," "
+
+mov al,6        
+mov edx,description1
+int 61h
 
 
 mov ecx,[descripteur_usb+4]
 and ecx,0FFh
+cmp ecx,0
+je charge_cfg_descriptor
 mov edx,classe
 mov al,105
 int 61h
 mov byte[edx+2],","
-
 
 mov ecx,[descripteur_usb+5]
 and ecx,0FFh
 mov edx,sousclasse
 mov al,105
 int 61h
+mov byte[edx+2],","
+
+mov ecx,[descripteur_usb+6]
+and ecx,0FFh
+mov edx,protocole
+mov al,105
+int 61h
 mov byte[edx+2],13
 
+mov al,6        
+mov edx,description2
+int 61h
+
+call affiche_nom_fabricant
+call affiche_nom_classe
+jmp passeusb
+
+
+;***********************si classe dans interface
+charge_cfg_descriptor:
+mov al,6        
+mov edx,CRLF
+int 61h
+
+call affiche_nom_fabricant
+
+
+;lit le descripteur de configuation
+mov eax,2
+mov bl,[adresse_usb]
+mov bh,0   ;terminaison
+mov dl,0   ;index du descripteur
+mov dh,2   ;type de descripteur (config)
+mov edi,descripteur_usb
+int 68h
+cmp eax,0
+jne passeusb
+
+
+
+
+
+;parcours les interfaces pour en lire la classe
+mov esi,descripteur_usb
+xor edi,edi
+xor eax,eax
+mov ax,[descripteur_usb+2]
+mov ebp,descripteur_usb
+add ebp,eax
+
+
+boucle_parcours_interface:
+cmp byte[esi+1],4
+jne ignore_descripteur
+
+mov ecx,edi
+mov edx,interface
+mov dword[edx],0
+mov al,102
+int 61h
+or dword[edx],20202020h
+
+mov al,6        
+mov edx,description3
+int 61h
+
+mov ecx,[esi+5]
+and ecx,0FFh
+mov edx,classe
+mov al,105
+int 61h
+mov byte[edx+2],","
+
+mov ecx,[esi+6]
+and ecx,0FFh
+mov edx,sousclasse
+mov al,105
+int 61h
+mov byte[edx+2],","
+
+mov ecx,[esi+7]
+and ecx,0FFh
+mov edx,protocole
+mov al,105
+int 61h
+mov byte[edx+2],13
+
+mov al,6        
+mov edx,description2
+int 61h
 
 
 mov al,6        
-mov edx,description
+mov edx,TAB
 int 61h
+call affiche_nom_classe
+
+inc edi
+
+ignore_descripteur:
+xor eax,eax
+mov al,[esi]
+cmp eax,0
+je passeusb
+add esi,eax
+cmp esi,ebp
+jb boucle_parcours_interface 
 
 
 
 
 
-;**************************************************************
-;affiche la classe si on as accès a la base de donnée
-pushad
-mov ebx,bdd1
-mov ecx,[taille_bdd1]
-cmp ecx,0
-je passeligne
-add ecx,ebx
 
-
-;recherche la classe
-mov eax,20h
-shl eax,16
-mov ax,[classe]
-shl eax,8
-mov al,"+"
-
-boucle_recherche_classe:
-cmp [ebx],eax
-je affiche_classe
-inc ebx
-cmp ebx,ecx
-je passeligne
-jmp boucle_recherche_classe
-
-
-affiche_classe:
-push ebx
-push ecx
-mov edx,DEB
-mov eax,6
-int 61h
-mov edx,ebx
-add edx,4
-mov eax,6
-int 61h
-pop ecx
-pop ebx
-
-
-;recherche la sous classe
-mov eax,20h
-shl eax,16
-mov ax,[sousclasse]
-shl eax,8
-mov al,"."
-
-boucle_recherche_sousclasse:
-cmp [ebx],eax
-je affiche_sousclasse
-cmp word[ebx],02B00h
-je findeligne
-inc ebx
-cmp ebx,ecx
-je findeligne
-jmp boucle_recherche_sousclasse
-
-
-affiche_sousclasse:
-push ebx
-push ecx
-mov edx,VIRG
-mov eax,6
-int 61h
-mov edx,ebx
-add edx,4
-mov eax,6
-int 61h
-pop ecx
-pop ebx
-
-
-
-findeligne:
-mov edx,CRLF
-mov eax,6
-int 61h
-passeligne:
-popad
+;**********************************************************
+passeusb:
+inc byte[adresse_usb]
+cmp byte[adresse_usb],128
+jne bouclecmdlu
+int 60h
 
 
 
@@ -269,25 +301,22 @@ popad
 
 ;**************************************************************
 ;affiche le fabriquant/nom si on as accès a la base de donnée
+affiche_nom_fabricant:
 
-
-mov edx,description
-boucle:
-cmp byte[edx],0
-je fin
-
+;passe les lettre en minuscule
+mov edx,description1
+boucle_maj1:
 cmp byte[edx],"A"
 jb @f
 cmp byte[edx],"Z"
 ja @f
 add byte[edx],20h
 @@:
+cmp byte[edx],0
+je @f
 inc edx
-jmp boucle 
-
-fin:
-
-
+jmp boucle_maj1
+@@:
 
 
 pushad
@@ -360,16 +389,163 @@ int 61h
 
 passeligne2:
 popad
+ret
 
 
 
 
-;**********************************************************
-passeusb:
-inc byte[adresse_usb]
-cmp byte[adresse_usb],128
-jne bouclecmdlu
-int 60h
+
+
+
+
+
+;**************************************************************
+;affiche la classe si on as accès a la base de donnée
+affiche_nom_classe:
+;passe les lettre en minuscule
+mov edx,description2
+boucle_maj2:
+cmp byte[edx],"A"
+jb @f
+cmp byte[edx],"Z"
+ja @f
+add byte[edx],20h
+@@:
+cmp byte[edx],0
+je @f
+inc edx
+jmp boucle_maj2
+@@:
+
+
+
+
+
+
+
+
+pushad
+mov ebx,bdd1
+mov ecx,[taille_bdd1]
+cmp ecx,0
+je passeligne
+add ecx,ebx
+
+
+;recherche la classe
+mov eax,20h
+shl eax,16
+mov ax,[classe]
+shl eax,8
+mov al,"+"
+
+boucle_recherche_classe:
+cmp [ebx],eax
+je affiche_classe
+inc ebx
+cmp ebx,ecx
+je passeligne
+jmp boucle_recherche_classe
+
+
+affiche_classe:
+push ebx
+push ecx
+mov edx,DEB
+mov eax,6
+int 61h
+mov edx,ebx
+add edx,4
+mov eax,6
+int 61h
+pop ecx
+pop ebx
+
+
+;recherche la sous classe
+mov eax,20h
+shl eax,16
+mov ax,[sousclasse]
+shl eax,8
+mov al,"."
+
+boucle_recherche_sousclasse:
+cmp [ebx],eax
+je affiche_sousclasse
+cmp word[ebx],02B00h
+je findeligne
+inc ebx
+cmp ebx,ecx
+je findeligne
+jmp boucle_recherche_sousclasse
+
+
+affiche_sousclasse:
+push ebx
+push ecx
+mov edx,VIRG
+mov eax,6
+int 61h
+mov edx,ebx
+add edx,4
+mov eax,6
+int 61h
+pop ecx
+pop ebx
+
+;recherche protocole
+mov eax,20h
+shl eax,16
+mov ax,[protocole]
+shl eax,8
+mov al,"-"
+
+boucle_recherche_protocole:
+cmp [ebx],eax
+je affiche_protocole
+cmp word[ebx],02E00h
+je findeligne
+cmp word[ebx],02B00h
+je findeligne
+inc ebx
+cmp ebx,ecx
+je findeligne
+jmp boucle_recherche_protocole
+
+
+affiche_protocole:
+push ebx
+push ecx
+mov edx,VIRG
+mov eax,6
+int 61h
+mov edx,ebx
+add edx,4
+mov eax,6
+int 61h
+pop ecx
+pop ebx
+
+
+
+
+findeligne:
+mov edx,CRLF
+mov eax,6
+int 61h
+passeligne:
+popad
+ret
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -380,23 +556,36 @@ org 0
 msg2:
 db 13,"liste des périphériques USB détecté:",13,0 
 
-description:
-db "Adresse:"
+description1:
+db "adresse:"
 adresse:
 dd 0
-db "Vendor:"
+db "vendor:"
 vendor:
 dd 0
-db " ID:"
+db " id:"
 id:
 dd 0
-db " Classe:"
+db 0
+
+description2:
+db " classe:"
 classe:
 dw 0
-db ",Sous-Classe:"
+db ",sous-classe:"
 sousclasse:
 dw 0
+db ",protocole:"
+protocole:
+dw 0
 db 13,0
+
+
+description3:
+db "    interface:"
+interface:
+dd 0
+dd " ",0
 
 
 DEB:
@@ -405,7 +594,8 @@ VIRG:
 db ",",0
 CRLF:
 db 17h,13,0
-
+TAB:
+db "    ",0
 
 
 
