c39fdca79c8ff3af12243c8a63e722ec0d772b88
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2023-03-10 01:16:48 +0100

    change l'affichage de la liste des mots clefs

--- ASM/PROG/man.asm
+++ ASM/PROG/man.asm
@@ -71,6 +71,8 @@ mov ecx,[taille]
 add ecx,zt_recep+1
 mov al,8
 int 61h
+cmp eax,0
+jne aff_err_mem
 
 
 ;charge fichier
@@ -266,29 +268,368 @@ ret
 
 ;******************************************************************************************
 liste_motclef:
+
+;aggrandit la mem pour pouvoir créer une liste de mot clef
+mov ecx,[taille]
+sub ecx,zt_recep
+shr ecx,2  ;1/4 de la taille du fichier devrait être largement suffisant
+add ecx,[taille]
+mov dx,sel_dat1
+mov al,8
+int 61h
+cmp eax,0
+jne aff_err_mem
+
+
+;fait une liste des mots clefs
+mov ebx,zt_recep
+mov esi,[taille]
+
+boucle1_liste:
+cmp byte[ebx],":"
+jne suite1_liste
+inc ebx
+
+
+boucle2_liste:
+mov al,[ebx]
+cmp al,":"
+jne @f
+mov al,13
+@@:
+mov[esi],al
+inc ebx
+inc esi
+cmp ebx,[taille]
+je fin1_liste
+cmp byte[ebx],0
+jne boucle2_liste
+
+mov byte[esi],13
+inc esi
+
+suite1_liste:
+call atteint_ligne_suivante
+cmp ebx,[taille]
+jb boucle1_liste
+fin1_liste:
+mov dword[esi],0D0D0D0Dh
+mov dword[esi+4],0D0D0D0Dh
+mov byte[esi+8],0
+
+
+
+
+
+;compte les mots clefs
+xor eax,eax
+mov ebx,[taille]
+
+boucle_comptaille:
+cmp byte[ebx],13
+jne @f
+
+inc dword[nb_motclef]
+mov ecx,eax
+xor eax,eax
+cmp ecx,[max]
+jb @f
+mov [max],ecx
+
+@@:
+mov dl,[ebx]
+inc ebx
+and dl,0C0h
+cmp dl,80h
+je @f
+inc eax
+@@:
+cmp byte[ebx],0
+jne boucle_comptaille
+sub dword[nb_motclef],8
+
+
+
+mov al,2
+mov edx,table_info_vid
+int 63h
+mov eax,[table_info_vid+0Ch]
+mov [largeur],eax
+
+
+
+;nb colonne = largeur/(max+4)
+;nb_ligne = nb_motclef/nb colonne-1
+add dword[max],4
+xor eax,eax
+mov ax,[largeur]
+xor edx,edx
+mov ecx,[max]
+div ecx
+cmp eax,8 ;on limite les collonnes a 8
+jb @f
+mov eax,8
+@@:
+mov [nb_collonne],eax
+xor edx,edx
+mov ecx,eax
+mov eax,[nb_motclef]
+div ecx
+mov [nb_ligne],eax
+cmp edx,0
+je @f
+inc dword[nb_ligne]
+@@:
+
+
+shl dword[nb_collonne],2
+
+
+
+jmp sff_lit_dossier_trie_fin ;temp!!!!!!!!!!!!!!!!!!
+
+
+;trie les par ordre alphabetique
+mov esi,[taille]
+mov edi,[taille]
+
+
+sff_lit_dossier_trie_fichier_suivant:
+cmp byte[edi],13
+je @f
+cmp byte[edi],0
+je sff_lit_dossier_trie_fin
+inc edi
+jmp sff_lit_dossier_trie_fichier_suivant
+@@:
+inc edi
+
+;test si le fichier doit être placé avant et le déplace si nécessaire
+mov esi,edx
+sff_lit_dossier_trie_boucle:
+call sff_lit_dossier_test
+jnc @f
+;call sff_lit_dossier_decale
+jmp sff_lit_dossier_trie_fichier_suivant
+
+
+
+@@:
+cmp byte[esi],13
+je @f
+cmp byte[esi],0
+je sff_lit_dossier_trie_fichier_suivant
+inc esi
+jmp @b
+@@:
+inc esi
+cmp esi,edi
+je sff_lit_dossier_trie_fichier_suivant
+jmp sff_lit_dossier_trie_boucle
+
+
+
+sff_lit_dossier_trie_fin: 
+
+
+
+
+;remplit les index
+mov esi,[taille]
+mov edi,index
+mov ecx,[nb_ligne]
+mov [edi],esi
+add edi,4
+
+boucle_index:
+cmp byte[esi],0
+je fin_index
+cmp byte[esi],13
+jne @f
+dec ecx
+jnz @f
+inc esi
+mov [edi],esi
+add edi,4
+mov ecx,[nb_ligne]
+jmp boucle_index
+
+@@:
+inc esi
+jmp boucle_index
+
+fin_index:
+
+
+
+
+;affiche la liste
 mov al,6
 mov edx,msg6
 int 61h
 
-mov ebx,zt_recep
 
-boucle_liste:
-cmp byte[ebx],":"
-jne suite_liste
+boucle_ligne:
+;efface la ligne
+mov eax,20202020h
+mov edi,ligne_vide
+mov ecx,64
+rep stosd
 
+;insère les mots clef au bon endroit
+xor ebx,ebx
+mov edi,ligne_vide
+
+boucle_col:
+mov esi,[index+ebx]
+push edi
+@@:
+lodsb
+stosb
+cmp al,13
+jne @b
+dec edi
+mov byte[edi],20h
+pop edi
+mov [index+ebx],esi
+
+
+
+
+
+
+
+
+;passe a la colonne suivante
+mov ecx,[max]
+boucle_colonne_suiv:
+mov al,[edi]
+inc edi
+and al,0C0h
+cmp al,80h
+je @f
+dec ecx
+@@:
+cmp ecx,0
+jne boucle_colonne_suiv 
+
+add ebx,4
+cmp ebx,[nb_collonne]
+jne boucle_col
+
+
+
+fin_col:
+;ajoute fin
+dec edi
+mov word[edi],13
+
+;ecrit ligne
 mov al,6
-mov edx,ebx
+mov edx,ligne_vide
 int 61h
 
-suite_liste:
-call atteint_ligne_suivante
-cmp ebx,[taille]
-jb boucle_liste
+dec dword[nb_ligne]
+jnz boucle_ligne
 
+mov al,6
+mov edx,blanc
+int 61h
 mov al,6
 mov edx,msg_crlf
 int 61h
 int 60h
+
+
+
+
+;******************
+sff_lit_dossier_test:  ;cf=1 si le fichier en edi doit se placer avant celuis en esi
+pushad
+sff_lit_dossier_test_boucle:
+mov al,[edi]
+mov ah,[esi]
+cmp al,"a"
+jb @f
+cmp al,"z"
+ja @f
+sub al,"a"-"A"
+@@:
+cmp ah,"a"
+jb @f
+cmp ah,"z"
+ja @f
+sub ah,"a"-"A"
+@@:
+cmp al,ah
+jb sff_lit_dossier_test_ok
+jne sff_lit_dossier_test_nok
+inc edi
+inc esi
+jmp sff_lit_dossier_test_boucle
+
+sff_lit_dossier_test_ok:
+popad
+stc
+ret
+
+sff_lit_dossier_test_nok:
+popad
+clc
+ret
+
+
+
+
+;***********
+sff_lit_dossier_decale:
+pushad
+;calcul taille a deplacer
+mov edx,edi
+mov ecx,edi
+sub edx,2
+sub ecx,esi
+
+
+;sauvegarde nom a decaler
+push word 8000h
+xor eax,eax
+@@:
+mov al,[edi]
+cmp al,0
+je @f
+cmp al,13
+je @f
+push ax
+inc edi
+jmp @b
+@@:
+dec edi
+
+;décale les noms
+dec ecx
+mov esi,edx
+std
+rep movsb
+
+;recopie nom sauvegardé
+mov byte[edi],13
+dec edi
+@@:
+pop ax
+cmp ax,8000h
+je @f
+mov [edi],al
+dec edi
+jmp @b
+@@:
+
+popad
+ret
+
+
+
+
 
 
 
@@ -322,7 +663,15 @@ aff_err_fichier:
 mov al,6
 mov edx,msg4
 int 61h
-int 60h  
+int 60h
+
+
+aff_err_mem:
+mov al,6
+mov edx,msg8
+int 61h
+int 60h
+
 
 ;*******************************************************
 sdata1:   ;données dans le segment de donnée N°1
@@ -333,15 +682,17 @@ db 13,"MAN: aucune entrée dans le manuel concernant ",22h,0
 msg2:
 db 22h,13,0
 msg3:
-db 13,"MAN: erreur dans les parametres de la motclef",13,0
+db 13,"MAN: erreur dans les parametres du motclef",13,0
 msg4:
 db 13,"MAN: erreur d'acces au fichier du manuel",13,0
 msg5:
 db 13,"MAN: contenu de la rubrique ",22h,0
 msg6:
-db 13,"MAN: liste des rubriques disponibles:",13,0
+db 13,"MAN: liste des rubriques disponibles:",13,13h,0
 msg7:
 db " ",0
+msg8:
+db 13,"MAN: erreur de reservation mémoire",13,0
 
 msg_crlf:
 db 13,17h,0
@@ -350,6 +701,27 @@ blanc:
 db 1Fh,0
 vert:
 db 1Ah,0
+
+
+largeur:
+dd 0
+nb_collonne:
+dd 0
+nb_motclef:
+dd 0
+max:
+dd 0
+nb_ligne:
+dd 0
+
+index:
+rb 256
+
+
+table_info_vid:
+rb 40
+ligne_vide:
+rb 512
 
 
 fichier:
@@ -361,6 +733,7 @@ dd 0,0
 
 motclef:
 rb 128
+
 zt_recep:
 rb 512
 
