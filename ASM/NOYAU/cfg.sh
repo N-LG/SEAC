998462bc46b384f30f5d8ba38ca80dd983bb363a
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2024-04-17 23:25:04 +0200

    petites modif pour faciliter l'installation a partir d'une image bootable

---
+++ ASM/NOYAU/cfg.sh
@@ -0,0 +1,17 @@
+modv 800*600
+#def en-txt.def
+#def en-qwi.def
+pilote pci
+*
+stftp #dm -w
+shttp #dm
+stlnt
+cdns
+ipconfig 0 comp 192.168.1.200 255.255.255.0 192.168.1.1 0:0:0:0:0:0:0:0 0:0:0:0:0:0:0:0
+#ipconfig 1 comp 192.168.1.201 255.255.255.0 192.168.1.1 0:0:0:0:0:0:0:0 0:0:0:0:0:0:0:0
+*
+af
+#af pour un rappel des principales commandes disponible
+#af entrez la commande «man commandes»
+af for a reminder of the available commands
+af enter the command "man command"
