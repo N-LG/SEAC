6a8c1a803ec168111bef3e0535cbd11b7ae440df
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2026-05-30 23:06:06 +0200

    correction bug copier/coller et int 3

--- ASM/NOYAU/cfg.sh
+++ ASM/NOYAU/cfg.sh
@@ -2,15 +2,16 @@ dcp CFG.ZIP
 modv 800*600
 #def en-txt.def
 #def en-qwi.def
+**
+ics
 pilote pci
-*
+**
 stftp #dm -w
 shttp #dm
 stlnt
 cdns
 ipconfig 0 comp 192.168.1.200 255.255.255.0 192.168.1.1 0:0:0:0:0:0:0:0 0:0:0:0:0:0:0:0
 #ipconfig 1 comp 192.168.1.201 255.255.255.0 192.168.1.1 0:0:0:0:0:0:0:0 0:0:0:0:0:0:0:0
-*
 af
 #af pour un rappel des principales commandes disponible
 #af entrez la commande «man commandes»
